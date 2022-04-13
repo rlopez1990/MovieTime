a9f167f85363129db83c101d6e3bbfe352e3a5c0
Raul Lopez Martinez <raul_lopez@gap.com>
2022-04-12 20:52:23 -0500

    Initial commit

---
+++ MovieTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTime
+//
+//  Created by Raul Lopez Martinez on 12/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
