b49687f7a68a70b238ed8de384f2710329f79206
Raul Lopez Martinez <raul_lopez@gap.com>
2022-04-15 21:02:47 -0500

    Add icon and place holder

---
+++ MovieTime/Networking/ImageCache.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCache.swift
+//  MovieTime
+//
+//  Created by Raul Lopez Martinez on 15/04/22.
+//
+
+import Foundation
---
+++ MovieTime/Networking/Protocol/MainThreadExecutable.swift
@@ -0,0 +1,8 @@
+//
+//  MainThreadExecutable.swift
+//  MovieTime
+//
+//  Created by Raul Lopez Martinez on 15/04/22.
+//
+
+import Foundation
